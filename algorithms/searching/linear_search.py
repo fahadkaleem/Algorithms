e795c6754335b846f549005f6522dde5bc589791
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2016-09-25 13:22:16 -0400

    Added docstring

--- algorithms/searching/linear_search.py
+++ algorithms/searching/linear_search.py
@@ -1,10 +1,25 @@
 """
-Author: Mohammed Fahad Kaleem
+    Linear Search Algorithm
+    -----------------------
+    Compare each element in the input list until the 'key' is found
+
+    Time Complexity: O(n) where n is the number of elements in the input list
+
+    More details about Linear Search on https://en.wikipedia.org/wiki/Linear_search
 """
-def search(inputList, elementToBeFound):
+__author__ = 'Mohammed Fahad Kaleem'
+
+def search(inputList, key):
+    """
+    Takes a list and searches if 'key' is in the list
+    :param inputList: A list with elements
+    :param key: Element that needs to be searched for
+    :return: Returns position if the 'key' is found
+             Returns False if the 'key' is not found
+    """
     position = 0
     while position < len(inputList):
-        if inputList[position] == elementToBeFound:
-            return True
+        if inputList[position] == key:
+            return position
         position += 1
     return False
--- tests/linearsearch_test.py
+++ tests/linearsearch_test.py
@@ -12,15 +12,26 @@ from algorithms.searching import (
 class TestLinearSearch(unittest.TestCase):
     def setUp(self):
         self.inputList = [2,65,12,90,123,76,71]
+        self.stringList = ['cat','apple','orange','bananan']
+        self.emptyList = []
         self.elementInList = 90
         self.elementNotInList = 17
+        self.string = 'orange'
 
     def test_linear_search_correct_result(self):
-        self.assertEqual(True, linear_search.search(self.inputList, self.elementInList))
-
+        self.assertEqual(3, linear_search.search(self.inputList, self.elementInList))
 
     def test_linear_search_incorrect_result(self):
         self.assertEqual(False, linear_search.search(self.inputList, self.elementNotInList))
+
+    def test_linear_search_emptyList_result(self):
+        self.assertEqual(False, linear_search.search(self.emptyList, self.elementInList))
+
+    def test_linear_search_stringKey_IntegerList_result(self):
+        self.assertEqual(False, linear_search.search(self.inputList, self.string))
+
+    def test_linear_search_stringType_result(self):
+        self.assertEqual(2, linear_search.search(self.stringList, self.string))
 
 if __name__ == "__main__":
     unittest.main()
